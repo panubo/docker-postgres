01b1817dacfa691051a82e0fb2e833d4dd301dab
Tim Robinson <tim@panubo.com>
2017-12-11 17:01:50 +1100

    Add postgres 9.6 image

---
+++ 9.6-debian/dump.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+# dirs
+BACKUP_PATH=/srv/pg_dump
+BACKUP_DIR="${BACKUP_PATH}/$(date +%Y%m%d%H%M%S)"
+
+PG_BIN=/usr/lib/postgresql/9.6/bin
+
+# Configs
+DBS=$(su -l postgres -c "${PG_BIN}/psql -At -c \"SELECT datname FROM pg_database WHERE NOT datistemplate\"")
+mkdir -p ${BACKUP_DIR} ${BACKUP_PATH}/latest
+
+# globals
+su -l postgres -c "${PG_BIN}/pg_dumpall --globals-only" | gzip > ${BACKUP_DIR}/globals.sql.gz;
+ln -f ${BACKUP_DIR}/globals.sql.gz ${BACKUP_PATH}/latest/globals.sql.gz;
+
+# DB's
+for DB in $DBS; do
+    # echo "Dumping ${DB}";
+    su -l postgres -c "${PG_BIN}/pg_dump -Fp ${DB}" | gzip > ${BACKUP_DIR}/${DB}.sql.gz;
+    ln -f ${BACKUP_DIR}/${DB}.sql.gz ${BACKUP_PATH}/latest/${DB}.sql.gz;
+done
+
+# find ${BACKUP_PATH} -type d -mtime +{{postgres_backup_keep}} -prune -exec rm -rf {} \;
