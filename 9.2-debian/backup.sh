2c4da9722b7b1f5907f9d2680345bb5365eda7c1
Andrew Cutler <andrew@panubo.com>
2018-08-10 15:38:43 +1000

    Removed legacy 9.2 image

--- 9.2-debian/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-psql -U postgres -c "SELECT pg_start_backup('backup', true);"
-rsync -av --inplace --no-whole-file --exclude pg_xlog/* /var/lib/postgresql/data/ /srv/pg_backup/data/
-psql -U postgres -c "SELECT pg_stop_backup();"
-
