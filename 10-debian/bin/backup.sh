3d10acd865e470c37d41cdf7cac1f99cae6af4e9
Tim Robinson <tim@panubo.com>
2019-04-10 11:36:25 +1000

    Update backup scripts to close backup on rsync failure

--- 10-debian/bin/backup.sh
+++ 10-debian/bin/backup.sh
@@ -2,14 +2,22 @@
 
 # Rsync file level backup of Postgres data directory (excluding WAL)
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
 # dirs
-BACKUP_PATH="/srv/pg_backup/data"
+BACKUP_PATH="/srv/pg_backup"
 
 # checks
 [ ! -d "${BACKUP_PATH}" ] && { echo "Error: ${BACKUP_PATH} does not exist"; exit 128; }
 
 psql -U postgres -c "SELECT pg_start_backup('backup', true);"
-rsync -av --inplace --delete --no-whole-file --exclude pg_wal/* --exclude pg_xlog/* /var/lib/postgresql/data/ ${BACKUP_PATH}
-psql -U postgres -c "SELECT pg_stop_backup();"
+
+# Always stop the backup even on failure
+# Trap is later in the script as it is only required if the backup is started successfully
+finish() {
+  psql -U postgres -c "SELECT pg_stop_backup();"
+}
+trap finish EXIT
+
+rsync -av --inplace --delete --no-whole-file --exclude pg_wal/* --exclude pg_xlog/* /var/lib/postgresql/data/ ${BACKUP_PATH}/data/
--- 9.6-debian/bin/backup.sh
+++ 9.6-debian/bin/backup.sh
@@ -2,7 +2,8 @@
 
 # Rsync file level backup of Postgres data directory (excluding WAL)
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
 # dirs
 BACKUP_PATH="/srv/pg_backup"
@@ -11,5 +12,12 @@ BACKUP_PATH="/srv/pg_backup"
 [ ! -d "${BACKUP_PATH}" ] && { echo "Error: ${BACKUP_PATH} does not exist"; exit 128; }
 
 psql -U postgres -c "SELECT pg_start_backup('backup', true);"
+
+# Always stop the backup even on failure
+# Trap is later in the script as it is only required if the backup is started successfully
+finish() {
+  psql -U postgres -c "SELECT pg_stop_backup();"
+}
+trap finish EXIT
+
 rsync -av --inplace --delete --no-whole-file --exclude pg_wal/* --exclude pg_xlog/* /var/lib/postgresql/data/ ${BACKUP_PATH}/data/
-psql -U postgres -c "SELECT pg_stop_backup();"
