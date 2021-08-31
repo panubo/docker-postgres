e769bcf60bac7a0b0bfba2203d4ca61ac3c818d6
Tim Robinson <tim@panubo.com>
2021-08-31 12:48:05 +1000

    Add 11-buster image

---
+++ 11-buster/entry-init/configure-wal-g.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ "${WALG_ENABLED:-}" != "true" ]]; then
+  echo "Info: WALG_ENABLED false"
+  return 0
+fi
+
+if [[ -n "${AWS_ENDPOINT:-}" ]] && [[ "${AWS_S3_FORCE_PATH_STYLE:-}" != "true" ]]; then
+  echo "WARNING: looks like you are using a custom AWS_ENDPOINT but AWS_S3_FORCE_PATH_STYLE is not true, this may cause wal-g to fail to connect."
+fi
+
+if [[ -e "${PGDATA:-/var/lib/postgres/data}/PG_VERSION" ]]; then
+  conf_file="${PGDATA:-/var/lib/postgres/data}/postgresql.conf"
+else
+  conf_file="/usr/share/postgresql/postgresql.conf.sample"
+fi
+
+ls "${conf_file}"
+
+sed -i -e "s@^#archive_mode.*@archive_mode = on@g" "${conf_file}"
+sed -i -e "s@^#archive_command.*@archive_command = '/usr/bin/timeout 600 /usr/local/bin/wal-g wal-push %p'@g" "${conf_file}"
+sed -i -e "s@^#archive_timeout.*@archive_timeout = 600@g" "${conf_file}"
